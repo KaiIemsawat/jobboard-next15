3216e3225d71ad8b8f75a5978e4c29b72166c51d
Kai Iemsawat <kaiiemsawat@gmail.com>
2025-02-04 23:05:18 -0500

    refactored button svg

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 import Image from "next/image";
 
 import Logo from "@/public/logo.png";
-import { Button } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
 import { ThemeToggle } from "./ThemeToggle";
-import { auth } from "@/app/utils/auth";
+import { auth, signOut } from "@/app/utils/auth";
 
 export async function Navbar() {
   const session = await auth();
@@ -19,7 +19,24 @@ export async function Navbar() {
       </Link>
       <div className="flex items-center gap-4">
         <ThemeToggle />
-        {session?.user ? <Button>Logout</Button> : <Button>Login</Button>}
+        {session?.user ? (
+          <form
+            action={async () => {
+              "use server";
+
+              await signOut({ redirectTo: "/" });
+            }}
+          >
+            <Button variant="secondary">Logout</Button>
+          </form>
+        ) : (
+          <Link
+            href="/login"
+            className={buttonVariants({ variant: "default", size: "lg" })}
+          >
+            Login
+          </Link>
+        )}
       </div>
     </nav>
   );
