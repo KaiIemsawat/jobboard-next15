fa007bad7f889ebbaff6c92f467a1b96eff9c14d
Kai Iemsawat <kaiiemsawat@gmail.com>
2025-02-13 14:32:05 -0500

    Job information part 3. Job description header 1

--- components/richTextEditor/JobDescriptionEditor.tsx
+++ components/richTextEditor/JobDescriptionEditor.tsx
@@ -1,5 +1,6 @@
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { MenuBar } from "./MenuBar";
 
 export function JobDescriptionEditor() {
   const editor = useEditor({
@@ -8,6 +9,7 @@ export function JobDescriptionEditor() {
   });
   return (
     <div className="w-full border rounded-lg overflow-hidden bg-card">
+      <MenuBar editor={editor} />
       <EditorContent editor={editor} />
     </div>
   );
